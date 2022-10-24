a4e0e2769ba13d284dd78e885805e37170e56c67
Ivan Vodopianov <vis3010@gmail.com>
2022-10-24 15:00:53 +0300

    end Task-3 HW1 React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,29 @@
-export const App = ({ children }) => {
+import { Profile } from './Profile/Profile';
+import user from 'data/user.json';
+
+import {Statistics} from 'components/Statistics/Statistics';
+import data from 'data/data.json';
+
+import { FriendList } from './FriendList/FriendList';
+import friends from 'data/friends.json';
+
+import css from '../common/App.module.css';
+
+export const App = () => {
   return (
-    <div
-      style={{
-        // height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      {children}
+    <div className={css.main}>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+
+<Statistics title="Upload stats" stats={data} />
+<Statistics stats={data} />
+
+<FriendList friends={friends} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,25 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import {Profile} from 'components/Profile/Profile';
-import user from 'data/user.json';
-
-import {Statistics} from 'components/Statistics/Statistics';
-import data from 'data/data.json'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-    <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
-
-<Statistics title="Upload stats" stats={data} />
-<Statistics stats={data} />
-    </App>
+    <App/>
   </React.StrictMode>
 );
